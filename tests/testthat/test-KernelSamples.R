3d71556add1e00fc5f3adb1cceca3150d33e5c00
Vitor Pignatari <vitor.pignatari@ufpr.br>
2026-07-24 20:11:26 -0300

    some documentation corrections and errors too

--- tests/testthat/test-KernelSamples.R
+++ tests/testthat/test-KernelSamples.R
@@ -2,9 +2,9 @@ test_that("The class KernelSamples will not be a problem", {
   
   newObject <- KernelSamples(
     splitfun = stratifiedKfold,
-    splitargs = list(df = iris, K = 4, y = 'Species')
+    splitargs = list(df = iris, K = 4, y = 'Species', balanced = TRUE)
   )
-  
+
   expect_true(class(newObject) == 'KernelSamples')
   
 })
